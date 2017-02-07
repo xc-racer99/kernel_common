6243f7bbc1674d1ca5ff80fce7352760ad6bb9f0
xc-racer99 <xc-racer2@live.ca>
2017-02-07 15:59:53 -0800

    yaffs2: Major update
    Import from https://github.com/LegacyXperia/android_kernel_semc_msm7x30/commit/00bad58491345d51cc663d9fb77a474aa6521aa7

--- fs/yaffs2/yaffs_tagsmarshall.h
+++ fs/yaffs2/yaffs_tagsmarshall.h
@@ -13,27 +13,10 @@
  * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
  */
 
-/* This is used to pack YAFFS1 tags, not YAFFS2 tags. */
-
-#ifndef __YAFFS_PACKEDTAGS1_H__
-#define __YAFFS_PACKEDTAGS1_H__
+#ifndef __YAFFS_TAGSMARSHALL_H__
+#define __YAFFS_TAGSMARSHALL_H__
 
 #include "yaffs_guts.h"
+void yaffs_tags_marshall_install(struct yaffs_dev *dev);
 
-struct yaffs_packed_tags1 {
-	u32 chunk_id:20;
-	u32 serial_number:2;
-	u32 n_bytes:10;
-	u32 obj_id:18;
-	u32 ecc:12;
-	u32 deleted:1;
-	u32 unused_stuff:1;
-	unsigned should_be_ff;
-
-};
-
-void yaffs_pack_tags1(struct yaffs_packed_tags1 *pt,
-		      const struct yaffs_ext_tags *t);
-void yaffs_unpack_tags1(struct yaffs_ext_tags *t,
-			const struct yaffs_packed_tags1 *pt);
 #endif
